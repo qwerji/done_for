aa2937a6513b7c3bcc1f3affae33afd9113f1e5b
Ben Swanson <bentswanson@gmail.com>
2017-01-06 23:17:42 -0800

    restores original db name

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose'),
     path = require('path'),
     models_path = path.join(__dirname + './../models');
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/done_for');
+mongoose.connect('mongodb://localhost/doneFor');
 fs.readdirSync(models_path).forEach(function(file){
     if(file.indexOf('.js') >= 0){
         require(models_path + '/' + file);
