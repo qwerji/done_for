4c63b177e7de2773f6bbf74f67311f6c4409d2ea
qwerji <bentswanson@gmail.com>
2017-02-24 15:27:04 -0800

    updates location svgs

--- client/map.js
+++ client/map.js
@@ -107,7 +107,7 @@ const GameStateManager = function() {
                         {type: 'attr', text: 'Walk west into the Wastes', attr: 'w_wastes'},
                         {type: 'attr', text: 'Leave the Wastes', attr: 'is_in_wastes'}
                     ],
-                    img_url: 'https://kaskenkronicles.files.wordpress.com/2015/08/final-fantasy-jakerowell-envir-wasteland9.jpg'
+                    img_url: 'imgs/locations/wastes.svg'
                 }
                 // Replaces option to leave wastes with option to go south in the wastes, if coordinates are not [0, 0]
                 if (attributes['wastes_coords'][0] !== 0 || attributes['wastes_coords'][1] !== 0) {
@@ -204,7 +204,7 @@ const GameStateManager = function() {
                         {type: 'travel', text: 'Go West to the Lake', dest: 'Lake'},
                         {type: 'travel', text: 'Go East to the Crystal Forest', dest: 'Forest Exterior'}
                     ],
-                    img_url:'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+                    img_url:'imgs/locations/behind_ship.svg'
                 }
             }
             else if (attributes['monster_killed']) {
@@ -218,7 +218,7 @@ const GameStateManager = function() {
                         {type: 'death', text: 'Fight it', cause: 'Died bravely in battle.'},
                         {type: 'death', text: 'Run', cause: 'Died like a coward.'}
                     ],
-                    img_url:'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+                    img_url:'imgs/locations/monster.svg'
                 }
                 if (hero.inventory['red_stone']) {
                     situation.options[0] = {type: 'attr', text: 'Fight it', attr: 'monster_killed'}
@@ -240,7 +240,7 @@ const GameStateManager = function() {
                 situation = {
                     prompt: "After shining the flashlight in it's face, the Alien seems very irritated and won't acknowledge you. Oops.",
                     options: [{type: 'travel', text: 'Go Back', dest: 'Forest Interior'}],
-                    img_url:'http://www.hotel-r.net/im/hotel/ca/crystal-forest.jpg'
+                    img_url:'imgs/locations/alien.svg'
                 }
                 if (attributes['alien_rude']) {
                     situation.prompt = "After yelling at the Alien, it seems very irritated and won't acknowledge you. Oops."
@@ -269,14 +269,14 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Check Armory Cabinet', dest: 'Armory' },
                 { type: 'item', text: 'Put on EVA suit', name: 'eva' }
             ],
-            img_url: 'http://orig04.deviantart.net/fa92/f/2011/107/e/2/passage_1_by_penemenn-d3e9aoh.jpg'
+            img_url: 'imgs/locations/ship.svg'
         },
         'Ship Computer': {
             prompt: "You check the ship's computer, which tells you that the atmospheric conditions of the unknown planet you have landed on will not support human life. However, there are trace readings of other life forms.",
             options: [
                 { type: 'travel', text: 'Back to the ship main compartment', dest: 'Ship' }
             ],
-            img_url: 'http://ak6.picdn.net/shutterstock/videos/2038331/thumb/2.jpg'
+            img_url: 'imgs/locations/ship_computer.svg'
         },
         'Armory': {
             prompt: 'All the weapons appear to be unusable but there appears to be an undamaged flashlight.',
@@ -284,7 +284,7 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Back to the ship main compartment', dest: 'Ship' },
                 { type: 'item', text: 'Pick up the flashlight', name: 'flashlight' },
             ],
-            img_url: 'http://i.imgur.com/18Z47Ll.jpg'
+            img_url: 'imgs/locations/armory.svg'
         },
         'Crash Site': {
             prompt: 'Outside the ship you can see three landmarks.',
@@ -294,7 +294,7 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Go West to the Lake', dest: 'Lake' },
                 { type: 'travel', text: 'Go into the Ship', dest: 'Ship' }
             ],
-            img_url: 'https://s-media-cache-ak0.pinimg.com/originals/07/5c/37/075c37b3bf9051d109092713f3a60b13.jpg'
+            img_url: 'imgs/locations/crash_site.svg'
         },
         'Forest Exterior': {
             prompt: 'Standing at the entrance to the forest of crystals you notice a hole in one of the crystals nearby.',
@@ -303,7 +303,7 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Go behind the ship', dest: 'Behind Ship' },
                 { type: 'travel', text: 'Go to the crash site', dest: 'Crash Site' },
             ],
-            img_url: 'http://www.hotel-r.net/im/hotel/ca/crystal-forest.jpg'
+            img_url: 'imgs/locations/crystal_forest.svg'
         },
         'Forest Interior': {
             prompt: 'Inside the forest there are two paths that seem well traveled.',
@@ -312,7 +312,7 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Climb through the hole in the crystal', dest: "Alien's House" },
                 { type: 'travel', text: 'Leave the forest', dest: 'Forest Exterior' },
             ],
-            img_url: 'http://www.hotel-r.net/im/hotel/ca/crystal-forest.jpg'
+            img_url: 'imgs/locations/crystal_forest.svg'
         },
         'Flat Crystal Area': {
             prompt: 'You see a flat crystal on the ground.',
@@ -320,7 +320,7 @@ const GameStateManager = function() {
                 { type: 'item', text: 'Pick up the Flat Crystal', name: 'flat_crystal' },
                 { type: 'travel', text: 'Go Back', dest: 'Forest Interior' },
             ],
-            img_url: 'http://www.hotel-r.net/im/hotel/ca/crystal-forest.jpg'
+            img_url: 'imgs/locations/crystal_forest.svg'
         },
         "Alien's House": {
             prompt: 'After stepping through the hole, you come face to face with a strange looking Alien. He does not seem hostile, but is clearly wary of your presence.',
@@ -330,14 +330,14 @@ const GameStateManager = function() {
                 { type: 'attr', text: '"What the heck are you?!"', attr: 'alien_rude' },
                 { type: 'travel', text: 'Go Back', dest: 'Forest Interior' },
             ],
-            img_url: 'http://www.hotel-r.net/im/hotel/ca/crystal-forest.jpg'
+            img_url: 'imgs/locations/alien.svg'
         },
         'Get Red Stone': {
             prompt: 'You show it the Flat Crystal. He seems confused, and hits the Crystal out of your hand. The Alien then reveals a Red Stone, offering it to you.',
             options: [
                 { type: 'item/lose_item/travel', text: 'Take the Red Stone', name: 'red_stone', lost_name: 'flat_crystal', dest: "Alien's House" }
             ],
-            img_url: 'http://www.hotel-r.net/im/hotel/ca/crystal-forest.jpg'
+            img_url: 'imgs/locations/alien.svg'
         },
         'Wastes': {
             prompt: 'An expanse of wastelands unfolds in front of you.',
@@ -346,7 +346,7 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Go to the crash site', dest: 'Crash Site' },
                 { type: 'travel', text: 'Go West to the Lake', dest: 'Lake' }
             ],
-            img_url: 'https://kaskenkronicles.files.wordpress.com/2015/08/final-fantasy-jakerowell-envir-wasteland9.jpg'
+            img_url: 'imgs/locations/wastes.svg'
         },
         'Behind Ship': {
             prompt: "There's nothing here but some wreckage from the crash.",
@@ -354,7 +354,7 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Go West to the Lake', dest: 'Lake' },
                 { type: 'travel', text: 'Go East to the Crystal Forest', dest: 'Forest Exterior' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/behind_ship.svg'
         },
         'Lake': {
             prompt: "You think you notice a small hole in the bottom of the lake. Your EVA suit should allow you to enter the lake's mysterious liquid.",
@@ -363,7 +363,7 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Go behind the ship', dest: 'Behind Ship' },
                 { type: 'travel', text: 'Go to the crash site', dest: 'Crash Site' }
             ],
-            img_url: 'http://www.walldevil.com/wallpapers/a77/night-planet-sky-lake.jpg'
+            img_url: 'imgs/locations/lake.svg'
         },
         'Lake Hole': {
             prompt: 'You swim through the dark lake and once you reach the hole, two tunnels branch off from it. Both are quite dark.',
@@ -372,14 +372,14 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Take the right tunnel', dest: 'Lake Tunnel' },
                 { type: 'travel', text: 'Swim back to the surface', dest: 'Lake' }
             ],
-            img_url: 'http://www.walldevil.com/wallpapers/a77/night-planet-sky-lake.jpg'
+            img_url: 'imgs/locations/in_lake.svg'
         },
         'Inscriptions': {
             prompt: 'There are some inscriptions on the walls.',
             options: [
                 { type: 'travel', text: 'Continue through the tunnel', dest: 'Lake Tunnel' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/hint.svg'
         },
         'Lake Tunnel': {
             prompt: 'You reach two more tunnels.',
@@ -387,7 +387,7 @@ const GameStateManager = function() {
                 { type: 'death', text: 'Take the pitch-black left tunnel', cause: 'Killed by something in the dark.' },
                 { type: 'travel', text: 'Take the right tunnel', dest: 'Crystal Cavern' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/in_lake.svg'
         },
         'Crystal Cavern': {
             prompt: 'You enter a gleaming cavern, with clusters of crystals clinging to every surface. There is a green stone on the ground in front of you. There is an opening on the left wall that looks like you could squeeze your way into. You also notice a crystal stairway to your right.',
@@ -398,21 +398,21 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Enter the opening', dest: 'Cavern Opening' },
                 { type: 'attr/travel', text: 'Climb the stairway', dest: 'Forest Exterior', attr: 'used_staircase' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/crystal_cavern.svg'
         },
         'Cavern Opening': {
             prompt: 'You are squeezed into the opening in the wall. The opening continues deeper but it seems to get smaller as it goes.',
             options: [
                 { type: 'attr', text: 'Keep going', attr: 'opening_inc' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/crystal_cavern.svg'
         },
         'Crystal Search': {
             prompt: 'You walk over to a group of crystals to further examine them. As you do, you notice a change in the density of the ground. You push the alien soil around with your feet and uncover a metal door in the ground. It seems to be locked.',
             options: [
                 { type: 'travel', text: 'Go back', dest: 'Crystal Cavern' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/crystal_cavern.svg'
         },
         'Base': {
             prompt: 'The hole in the door you made with your Digger Laser is just big enough to climb through. You lower yourself down and land in a dimly lit hallway. There is a door at the end of the hallway, with a pitch-black, open doorway on the right.',
@@ -421,7 +421,7 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Enter the pitch-black open doorway', dest: 'Dark Room' },
                 { type: 'travel', text: 'Go back', dest: 'Crystal Cavern' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/base.svg'
         },
         'Dark Room': {
             prompt: 'You enter the Dark Room, shining your flashlight in front of you. Your stomach flips when you see a group of horrible looking aliens huddled in the corner. They simultaneously turn, make a blood-curdling sound and lunge towards you.',
@@ -429,7 +429,7 @@ const GameStateManager = function() {
                 { type: 'death', text: 'Fight them', cause: 'Killed by horrible monsters.' },
                 { type: 'travel', text: 'Run', dest: 'Command Center' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/hallway.svg'
         },
         'Hallway': {
             prompt: 'You reach another hallway, with another pitch-black doorway to your right. On your left, there is another door. You begin to hear some terrifying noises coming from an indiscernable location.',
@@ -437,7 +437,7 @@ const GameStateManager = function() {
                 { type: 'death', text: 'Enter the pitch-black doorway', cause: 'Killed by horrible monsters.' },
                 { type: 'travel', text: 'Enter the left door', dest: 'Command Center' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/base.svg'
         },
         'Command Center': {
             prompt: 'After running down the hallway in fear, you enter a door on your left, which seals behind you. Looking around, it seems as though you are in a command center of some sort. There is a strange looking machine on your left, and another room on your right. In front of you is a console with several switches on it.',
@@ -446,14 +446,14 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Enter the right room', dest: 'Book Room' },
                 { type: 'travel', text: 'Walk up to the console', dest: 'Console' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/command_center.svg'
         },
         'Inactive Teleporter': {
             prompt: 'You approach the dormant machine. There is a chamber, just big enough for someone to stand inside, and a series of controls within reach of it.',
             options: [
                 { type: 'travel', text: 'Go back', dest: 'Command Center' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/teleporter.svg'
         },
         'Book Room': {
             prompt: 'You enter the room, finding it to be strewn with scraps of paper with writing on them, written in a language you do not understand.',
@@ -461,14 +461,14 @@ const GameStateManager = function() {
                 { type: 'travel', text: 'Look at a random paper', dest: 'Book Hint' },
                 { type: 'travel', text: 'Go back to the main room', dest: 'Command Center' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/book_room.svg'
         },
         'Book Hint': {
-            prompt: 'Hint here.',
+            prompt: 'Interesting.',
             options: [
                 { type: 'travel', text: 'Set the paper down', dest: 'Book Room' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/hint.svg'
         },
         'Console': {
             prompt: 'The console has three switches in the middle that stand out to you, with a larger button below them.',
@@ -478,7 +478,7 @@ const GameStateManager = function() {
                 { type: 'attr', text: 'Flip the Middle Switch', attr: 'middle_switch' },
                 { type: 'travel', text: 'Step away from the console', dest: 'Command Center' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: ''
         },
         'Active Command Center': {
             prompt: 'The command center comes to life around you, lights turning on and machines humming. The strange machine in the corner appears to have been activated as well. A compartment on the console opens up the console, presenting you with a blue stone.',
@@ -486,7 +486,7 @@ const GameStateManager = function() {
                 { type: 'item', text: 'Take the Blue Stone', name: 'blue_stone' },
                 { type: 'travel', text: 'Step into the strange machine', dest: 'Wastes' }
             ],
-            img_url: 'http://i0.kym-cdn.com/photos/images/original/000/581/296/c09.jpg'
+            img_url: 'imgs/locations/teleporter.svg'
         }
     }
 }
